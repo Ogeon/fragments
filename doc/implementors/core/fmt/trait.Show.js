b721d0c08dc6160071a05d2b0a23da802749e67a
Travis <travis@travis-ci.org>
2014-05-26 13:24:07 +0000

    Update docs from Travis build 25

---
+++ doc/implementors/core/fmt/trait.Show.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['fragments'] = {Template:"fragments/struct.Template.html",ContentType:"fragments/type.ContentType.html",};
+
+                if (window.register_implementors) {
+                    window.register_implementors(implementors);
+                } else {
+                    window.pending_implementors = implementors;
+                }
+            
+})()
