b3db2ce5cff14d43464d393eabb2530983ec785a
Travis <travis@travis-ci.org>
2014-06-05 10:47:16 +0000

    Update docs from Travis build 29

--- doc/implementors/core/fmt/trait.Show.js
+++ doc/implementors/core/fmt/trait.Show.js
@@ -1,10 +1,10 @@
 (function() {var implementors = {};
-implementors['fragments'] = {Template:"fragments/struct.Template.html",ContentType:"fragments/type.ContentType.html",};
+implementors['fragments'] = ["impl <a class='trait' href='http://doc.rust-lang.org/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='type' href='fragments/type.ContentType.html' title='fragments::ContentType'>ContentType</a>","impl <a class='trait' href='http://doc.rust-lang.org/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='fragments/struct.Template.html' title='fragments::Template'>Template</a>",];
 
-                if (window.register_implementors) {
-                    window.register_implementors(implementors);
-                } else {
-                    window.pending_implementors = implementors;
-                }
-            
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
 })()
--- doc/implementors/std/from_str/trait.FromStr.js
+++ doc/implementors/std/from_str/trait.FromStr.js
@@ -1,10 +1,10 @@
 (function() {var implementors = {};
-implementors['fragments'] = {Template:"fragments/struct.Template.html",};
+implementors['fragments'] = ["impl <a class='trait' href='http://doc.rust-lang.org/std/from_str/trait.FromStr.html' title='std::from_str::FromStr'>FromStr</a> for <a class='struct' href='fragments/struct.Template.html' title='fragments::Template'>Template</a>",];
 
-                if (window.register_implementors) {
-                    window.register_implementors(implementors);
-                } else {
-                    window.pending_implementors = implementors;
-                }
-            
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
 })()
