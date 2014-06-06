b4afec2c1ab5d03e26666fe340105caa76970595
Travis <travis@travis-ci.org>
2014-06-06 08:39:47 +0000

    Update docs from Travis build 29

--- doc/implementors/fragments/trait.Generator.js
+++ doc/implementors/fragments/trait.Generator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['fragments'] = ["impl <a class='trait' href='fragments/trait.Generator.html' title='fragments::Generator'>Generator</a> for fn(args: &amp;<a class='struct' href='http://doc.rust-lang.org/std/vec/struct.Vec.html' title='std::vec::Vec'>Vec</a>&lt;<a class='struct' href='http://doc.rust-lang.org/std/string/struct.String.html' title='std::string::String'>String</a>&gt;) -&gt; <a class='struct' href='http://doc.rust-lang.org/alloc/owned/struct.Box.html' title='alloc::owned::Box'>Box</a>&lt;<a class='trait' href='http://doc.rust-lang.org/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a>&gt;",];
+implementors['fragments'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
