ee96aa8ef11cf7eb4c2a0e99220f490449a06d4b
Travis <travis@travis-ci.org>
2014-07-05 07:04:32 +0000

    Update docs from Travis build 33

--- doc/implementors/std/from_str/trait.FromStr.js
+++ doc/implementors/std/from_str/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['fragments'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/std/from_str/trait.FromStr.html' title='std::from_str::FromStr'>FromStr</a> for <a class='struct' href='fragments/struct.Template.html' title='fragments::Template'>Template</a>",];
+implementors['fragments'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/0.11.0/std/from_str/trait.FromStr.html' title='std::from_str::FromStr'>FromStr</a> for <a class='struct' href='fragments/struct.Template.html' title='fragments::Template'>Template</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
