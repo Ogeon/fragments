c182899eba1558aa0e309b2b77134e73139bafd6
Travis <travis@travis-ci.org>
2014-07-13 12:48:27 +0000

    Update docs from Travis build 35

--- doc/implementors/core/cmp/trait.PartialEq.js
+++ doc/implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['fragments'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/0.11.0/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='type' href='fragments/type.Token.html' title='fragments::Token'>Token</a>",];
+implementors['fragments'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='type' href='fragments/type.Token.html' title='fragments::Token'>Token</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
