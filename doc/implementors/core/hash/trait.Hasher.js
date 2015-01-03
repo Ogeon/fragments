89390704d64b7c90365f964716160fd948c5a20d
Travis <travis@travis-ci.org>
2015-01-03 20:44:36 +0000

    Update docs from Travis build 39

---
+++ doc/implementors/core/hash/trait.Hasher.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['fragments'] = ["<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/hash/trait.Hasher.html' title='core::hash::Hasher'>Hasher</a>&lt;<a class='struct' href='http://doc.rust-lang.org/nightly/core/hash/sip/struct.SipState.html' title='core::hash::sip::SipState'>SipState</a>&gt; for <a class='struct' href='http://doc.rust-lang.org/nightly/std/hash/struct.RandomSipHasher.html' title='std::hash::RandomSipHasher'>RandomSipHasher</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/core/ops/trait.Neg.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['fragments'] = ["<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a>&lt;<a class='struct' href='http://doc.rust-lang.org/nightly/std/time/duration/struct.Duration.html' title='std::time::duration::Duration'>Duration</a>&gt; for <a class='struct' href='http://doc.rust-lang.org/nightly/std/time/duration/struct.Duration.html' title='std::time::duration::Duration'>Duration</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
