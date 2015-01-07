eb409bb0a7f7f3c40fbb5e495e0e333d5e266223
Travis <travis@travis-ci.org>
2015-01-07 12:11:07 +0000

    Update docs from Travis build 45

--- doc/implementors/core/slice/trait.AsSlice.js
+++ doc/implementors/core/slice/trait.AsSlice.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['fragments'] = ["<a class='stability Experimental' title='Experimental: will be replaced by slice syntax'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/slice/trait.AsSlice.html' title='core::slice::AsSlice'>AsSlice</a>&lt;T&gt; for <a class='struct' href='http://doc.rust-lang.org/nightly/std/c_vec/struct.CVec.html' title='std::c_vec::CVec'>CVec</a>&lt;T&gt;","<a class='stability Experimental' title='Experimental: will be replaced by slice syntax'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/slice/trait.AsSlice.html' title='core::slice::AsSlice'>AsSlice</a>&lt;T&gt; for <a class='struct' href='http://doc.rust-lang.org/nightly/collections/vec/struct.Vec.html' title='collections::vec::Vec'>Vec</a>&lt;T&gt;",];
+implementors['fragments'] = ["<a class='stability Experimental' title='Experimental: will be replaced by slice syntax'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/slice/trait.AsSlice.html' title='core::slice::AsSlice'>AsSlice</a>&lt;T&gt; for <a class='struct' href='http://doc.rust-lang.org/nightly/collections/vec/struct.Vec.html' title='collections::vec::Vec'>Vec</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
