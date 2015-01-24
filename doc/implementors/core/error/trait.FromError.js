109ce440023257ffb39f6119964b87712ac5c52a
Travis <travis@travis-ci.org>
2015-01-24 00:26:44 +0000

    Update docs from Travis build 48

---
+++ doc/implementors/core/error/trait.FromError.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['fragments'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/error/trait.FromError.html' title='core::error::FromError'>FromError</a>&lt;<a class='struct' href='http://doc.rust-lang.org/nightly/std/sync/poison/struct.PoisonError.html' title='std::sync::poison::PoisonError'>PoisonError</a>&lt;T&gt;&gt; for <a class='enum' href='http://doc.rust-lang.org/nightly/std/sync/poison/enum.TryLockError.html' title='std::sync::poison::TryLockError'>TryLockError</a>&lt;T&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;'a, E&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/error/trait.FromError.html' title='core::error::FromError'>FromError</a>&lt;E&gt; for <a class='struct' href='http://doc.rust-lang.org/nightly/alloc/boxed/struct.Box.html' title='alloc::boxed::Box'>Box</a>&lt;<a class='trait' href='http://doc.rust-lang.org/nightly/core/error/trait.Error.html' title='core::error::Error'>Error</a> + 'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
