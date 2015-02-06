ac58fdde2c0587abc3ffd7623761c87c9b41af20
Travis <travis@travis-ci.org>
2015-02-06 21:01:39 +0000

    Update docs from Travis build 51

--- doc/implementors/core/ops/trait.Div.js
+++ doc/implementors/core/ops/trait.Div.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['fragments'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Div.html' title='core::ops::Div'>Div</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.i32.html'>i32</a>&gt; for <a class='struct' href='http://doc.rust-lang.org/nightly/std/time/duration/struct.Duration.html' title='std::time::duration::Duration'>Duration</a>",];
+implementors['fragments'] = ["<a class='stability Unstable' title='Unstable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Div.html' title='core::ops::Div'>Div</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.i32.html'>i32</a>&gt; for <a class='struct' href='http://doc.rust-lang.org/nightly/std/time/duration/struct.Duration.html' title='std::time::duration::Duration'>Duration</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
