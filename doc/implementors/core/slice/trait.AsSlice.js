ab48022ae39ed12e935d36e1e5ce177606bc97ac
Travis <travis@travis-ci.org>
2015-03-29 08:35:02 +0000

    Update docs from Travis build 57

--- doc/implementors/core/slice/trait.AsSlice.js
+++ doc/implementors/core/slice/trait.AsSlice.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['fragments'] = ["<a class='stability Deprecated' title='Deprecated: use std::convert::AsRef&lt;[T]&gt; instead'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/slice/trait.AsSlice.html' title='core::slice::AsSlice'>AsSlice</a>&lt;T&gt; for <a class='struct' href='http://doc.rust-lang.org/nightly/collections/vec/struct.Vec.html' title='collections::vec::Vec'>Vec</a>&lt;T&gt;",];
+implementors['fragments'] = ["<a class='stability Deprecated' title='Deprecated: use &amp;mut s[..] instead'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/slice/trait.AsSlice.html' title='core::slice::AsSlice'>AsSlice</a>&lt;T&gt; for <a class='struct' href='http://doc.rust-lang.org/nightly/collections/vec/struct.Vec.html' title='collections::vec::Vec'>Vec</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
